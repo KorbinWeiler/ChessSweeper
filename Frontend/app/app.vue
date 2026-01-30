d359ddec972933195bf676e4190156c15f74fa79
KorbinWeiler <korbinweiler@gmail.com>
2026-01-30 11:24:41 -0800

    Initial project setup

---
+++ Frontend/app/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>My Nuxt App</v-toolbar-title>
+    </v-app-bar>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </v-app>
+</template>
