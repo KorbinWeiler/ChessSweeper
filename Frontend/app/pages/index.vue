cfa63587c10a71586efeb45e4b7d5b65ca6fbf4b
KorbinWeiler <korbinweiler@gmail.com>
2026-01-30 23:38:24 -0800

    Changed "Start New Game" button to send the user to the setup game page

--- Frontend/app/pages/index.vue
+++ Frontend/app/pages/index.vue
@@ -3,7 +3,7 @@
     <v-container class="pa-5" fluid fill-height>
       <v-row align="center" justify="center" class="fill-height">
         <v-col cols="auto" class="text-center">
-          <v-btn to="/game" color="primary" large>Start a new Game</v-btn>
+          <v-btn to="/setupgame" color="primary" large>Start a new Game</v-btn>
           <h1>ChessSweeper Rules coming soon?</h1>
         </v-col>
       </v-row>
