d90ca472e0a0b5463c8448f5620af105084d1895
KorbinWeiler <korbinweiler@gmail.com>
2026-01-30 19:28:54 -0800

    setup file structure to use nuxt router

--- Frontend/app/app.vue
+++ Frontend/app/app.vue
@@ -2,8 +2,12 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <v-toolbar-title>My Nuxt App</v-toolbar-title>
+      <NuxtLink to="/" class="ml-5 mr-3" exact>
+        Home
+      </NuxtLink>
     </v-app-bar>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <v-main>
+      <NuxtPage/>
+    </v-main>
   </v-app>
 </template>
