b277e6baabff12d1f6e48a605ef448b24b7198a1
KorbinWeiler <korbinweiler@gmail.com>
2026-02-12 19:26:46 -0800

    feat: update chessception and shuffle chess options components with new UI elements

--- Frontend/app/components/chessception.vue
+++ Frontend/app/components/chessception.vue
@@ -1,3 +1,13 @@
 <template>
-    <h1>Chessception</h1>
+    <v-form>
+        <p>Dimensions: {{ Math.floor(dimensions) }}</p>
+        <v-slider v-model="dimensions" :min="1" :max="3" label="Dimensions">
+        </v-slider>
+    </v-form>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const dimensions = ref(1);
+</script>
--- Frontend/app/components/shuffle_chess_options.vue
+++ Frontend/app/components/shuffle_chess_options.vue
@@ -1,3 +1,14 @@
 <template>
-    <h1>Shuffle Chess Options</h1>
+    <v-form>
+        <v-radio-group class="setup-radios" v-model="radios">
+            <v-radio label="Shuffle by Piece Type" value="group" />
+            <v-radio label="Shuffle by Individual Piece" value="slide" />
+        </v-radio-group>
+    </v-form>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const radios = ref("group");
+</script>
